94065744c98a848d73576a804ab12b5f18157e73
Asim-Tahir <asimtahirbilgi@gmail.com>
2020-03-28 12:52:07 +0300

    Default vuex file structure

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+const {state} = require('./state');
+const {getters} = require('./getters');
+const {mutations} = require('./mutations');
+const {actions} = require('./actions');
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+	strict: true,
+
+	state,
+	getters,
+	mutations,	
+  actions
 })
