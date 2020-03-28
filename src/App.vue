088af314ce48177b5a6f1ccbe0dd6505a2520010
Asim-Tahir <asimtahirbilgi@gmail.com>
2020-03-28 12:48:42 +0300

    Remove default added external scss variable and default scss style

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<div id="app">
+		<router-view/>
+	</div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+	@import '@/scss/normalize.min.scss';
+	@import '@/scss/colors.scss';
+	@import url('https://fonts.googleapis.com/css?family=Quicksand:400,500,700&display=swap&subset=latin-ext');
 
-#nav {
-  padding: 30px;
+	body{
+		font-family: 'Quicksand', sans-serif;
+		font-size: 18pt;
+		background-color: $primary;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+		input{
+			font-family: inherit;
+			margin-bottom: 25px;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+			&:focus{
+				outline: none;
+			}
+		}
+	}
 </style>
